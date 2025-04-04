4b0b8fa2a2b7d8784a824cd966e3e0b619f27cb8
UvinduV <uvindusanketh470@gmail.com>
2025-04-04 09:43:51 +0530

    add to git

---
+++ src/test/java/lk/ijse/challengetest/ChallengeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.challengetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
