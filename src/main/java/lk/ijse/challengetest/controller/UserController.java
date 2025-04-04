41a6b4507629572ff4f091e7867663e62001e32f
UvinduV <uvindusanketh470@gmail.com>
2025-04-04 10:27:56 +0530

    create user sample

--- src/main/java/lk/ijse/challengetest/controller/UserController.java
+++ src/main/java/lk/ijse/challengetest/controller/UserController.java
@@ -1,4 +1,55 @@
 package lk.ijse.challengetest.controller;
 
+import lk.ijse.challengetest.dto.UserDTO;
+import lk.ijse.challengetest.exeption.DataPersistExeption;
+import lk.ijse.challengetest.service.UserService;
+import lk.ijse.challengetest.util.AppUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@RestController
+@RequestMapping("api/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveUser(
+            @RequestPart("name") String name,
+            @RequestPart("email") String email,
+            @RequestPart("phoneNo") String phoneNo,
+            @RequestPart("profilePic") MultipartFile profilePic
+    ){
+
+        String userId = AppUtil.generateUserId();
+        String picToBase64="";
+        try {
+            byte[] bytesProPic = profilePic.getBytes();
+            picToBase64= AppUtil.profilePicToBase64(bytesProPic);
+
+            var buildUserDTO=new UserDTO();
+            buildUserDTO.setUId(userId);
+            buildUserDTO.setName(name);
+            buildUserDTO.setEmail(email);
+            buildUserDTO.setProfilePic(picToBase64);
+
+            userService.createUser(buildUserDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+
+        }catch (DataPersistExeption e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
 }
--- src/main/java/lk/ijse/challengetest/service/UserServiceImpl.java
+++ src/main/java/lk/ijse/challengetest/service/UserServiceImpl.java
@@ -1,10 +1,15 @@
 package lk.ijse.challengetest.service;
 
+import jakarta.transaction.Transactional;
 import lk.ijse.challengetest.dto.UserDTO;
+import org.springframework.stereotype.Service;
 
+@Service
+@Transactional
 public class UserServiceImpl implements UserService{
     @Override
     public void createUser(UserDTO userDTO) {
+        System.out.println(userDTO);
 
     }
 
